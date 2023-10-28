e473b761bc589d54a0ecc43cf414d676bc5eb230
ALFANDI YULVIANO SAPUTRA <145526111+alfylvisptra@users.noreply.github.com>
2023-10-28 17:37:42 +0700

    Create integral_kelompok4.c

---
+++ tugas06/integral_kelompok4.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+double func(double x) {
+    return 6 * x * x + 5;
+}
+
+double trapezoidRule(double a, double b, int n) {
+    double h = (b - a) / n;
+    double integral = (func(a) + func(b)) / 2.0;
+    for (int i = 1; i < n; i++) {
+        double x = a + i * h;
+        integral += func(x);
+    }
+    return integral * h;
+}
+
+int main() {
+    double a = 1.0; // Batas bawah
+    double b = 3.0; // Batas atas
+    int n = 1000;   // Jumlah subinterval, bisa disesuaikan
+
+    double result = trapezoidRule(a, b, n);
+
+    printf("Hasil integral: %lf\n", result);
+
+    return 0;
+}
